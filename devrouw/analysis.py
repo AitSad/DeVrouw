aef4ca309368828cb30383e24a30ffe918d8ad3d
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2023-03-18 02:34:50 -0700

    Update analysis.py

--- devrouw/analysis.py
+++ devrouw/analysis.py
@@ -1,51 +1,45 @@
 import pandas as pd
 
+class Analysis:
+    def __init__(self, data: pd.Series):
+        self.data = data
+    
+    def describe_data(self):
+        """Returns basic statistics for the given data."""
+        return self.data.describe()
 
-def describe_data(data: pd.DataFrame):
-    """Returns basic statistics for the given data."""
-    return data.describe()
+    def calculate_mean(self):
+        """Calculates the mean of the given data."""
+        return self.data.mean()
 
+    def calculate_median(self):
+        """Calculates the median of the given data."""
+        return self.data.median()
 
-def calculate_mean(data: pd.Series):
-    """Calculates the mean of the given data."""
-    return data.mean()
+    def calculate_mode(self):
+        """Calculates the mode of the given data."""
+        return self.data.mode()[0]
 
+    def calculate_variance(self):
+        """Calculates the variance of the given data."""
+        return self.data.var()
 
-def calculate_median(data: pd.Series):
-    """Calculates the median of the given data."""
-    return data.median()
+    def calculate_standard_deviation(self):
+        """Calculates the standard deviation of the given data."""
+        return self.data.std()
 
+    def calculate_correlation(self, other_data: pd.Series):
+        """Calculates the correlation between two sets of data."""
+        return self.data.corr(other_data)
 
-def calculate_mode(data: pd.Series):
-    """Calculates the mode of the given data."""
-    return data.mode()[0]
+    def calculate_covariance(self, other_data: pd.Series):
+        """Calculates the covariance between two sets of data."""
+        return self.data.cov(other_data)
 
+    def calculate_percentile(self, percentile: float):
+        """Calculates the specified percentile of the given data."""
+        return self.data.quantile(percentile / 100)
 
-def calculate_variance(data: pd.Series):
-    """Calculates the variance of the given data."""
-    return data.var()
-
-
-def calculate_standard_deviation(data: pd.Series):
-    """Calculates the standard deviation of the given data."""
-    return data.std()
-
-
-def calculate_correlation(data1: pd.Series, data2: pd.Series):
-    """Calculates the correlation between two sets of data."""
-    return data1.corr(data2)
-
-
-def calculate_covariance(data1: pd.Series, data2: pd.Series):
-    """Calculates the covariance between two sets of data."""
-    return data1.cov(data2)
-
-
-def calculate_percentile(data: pd.Series, percentile: float):
-    """Calculates the specified percentile of the given data."""
-    return data.quantile(percentile / 100)
-
-
-def calculate_zscore(data: pd.Series):
-    """Calculates the z-score of the given data."""
-    return (data - data.mean()) / data.std()
+    def calculate_zscore(self):
+        """Calculates the z-score of the given data."""
+        return (self.data - self.data.mean()) / self.data.std()
